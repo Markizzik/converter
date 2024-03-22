a3b801e73d7610ee610802dfc3aa422eea60127d
backtrac3r <backtrac3r>
2024-03-22 08:42:34 +0300

    .

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,20 +4,24 @@ mod routes;
 
 use crate::app_data::AppData;
 use axum::{routing::get, Router};
-use clap::{App, Arg};
+use clap::{command, Arg};
 use routes::root;
 use std::{env, sync::Arc};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
+    let match_result = command!()
+        .arg(Arg::new("server_port").short('p'))
+        .get_matches();
+
     dotenvy::dotenv().unwrap();
 
     let app_data = Arc::new(AppData::new());
 
     let app = Router::new().route("/", get(root)).with_state(app_data);
 
-    let server_port = args.server_port;
+    let server_port: &u16 = match_result.get_one("server_port").unwrap();
     let listener = TcpListener::bind(&format!("0.0.0.0:{server_port}"))
         .await
         .unwrap();
