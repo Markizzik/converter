a37284f5294a61d4a750de81d17c3586d3686108
Markizzik <mark.shcherbakov.76@mail.ru>
2024-10-04 14:20:03 +0300

    .

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,14 @@
-# Используем официальный образ Python
 FROM python:3.9-slim
 
-# Устанавливаем зависимости для Pillow (работа с изображениями)
 RUN apt-get update && apt-get install -y \
     libjpeg-dev zlib1g-dev
 
-# Устанавливаем рабочую директорию в контейнере
 WORKDIR /app
 
-# Копируем файлы в рабочую директорию контейнера
 COPY . /app
 
-# Устанавливаем зависимости из requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Открываем порт 5000 для Flask
 EXPOSE 5000
 
-# Команда для запуска Flask-приложения
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -5,24 +5,20 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-# Путь для сохранения загруженных файлов и конвертированных файлов
 UPLOAD_FOLDER = 'uploads'
 CONVERTED_FOLDER = 'converted'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'bmp', 'tiff', 'webp'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['CONVERTED_FOLDER'] = CONVERTED_FOLDER
 
-# Создаём папки, если они не существуют
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 if not os.path.exists(CONVERTED_FOLDER):
     os.makedirs(CONVERTED_FOLDER)
 
-# Проверка допустимости расширения файла
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# Конвертация изображения в целевой формат
 def convert_image(input_path, output_format):
     output_file = os.path.splitext(input_path)[0] + f".{output_format}"
     with Image.open(input_path) as img:
@@ -32,7 +28,7 @@ def convert_image(input_path, output_format):
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # Используется ваш файл index.html
+    return render_template('index.html') 
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -46,7 +42,7 @@ def upload_file():
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         input_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(input_path)  # Сохранение загруженного файла в папку uploads
+        file.save(input_path)
 
         return f"Файл успешно загружен: {filename}"
 
@@ -64,12 +60,10 @@ def convert_file():
         return "Неподдерживаемый формат", 400
 
     if file and allowed_file(file.filename):
-        # Сохранение исходного файла в папку uploads
         filename = secure_filename(file.filename)
         input_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(input_path)
 
-        # Конвертация файла в нужный формат
         output_file = convert_image(input_path, format)
         
         return send_file(output_file, as_attachment=True, download_name=f'converted.{format}')
